ef32a784f720f325fe7344c6c646c717eef3a77b
Ben Wang <benwangott@mgail.com>
2026-04-26 02:59:48 -0400

    finished city lookup

--- server.js
+++ server.js
@@ -1,75 +1,45 @@
-async function startServer(port) {
-    const fs = await import('node:fs');
-    const weather = await import('openmeteo');
-    const parse = await import("jsr:@std/csv/parse");
-    const bodyParser = await import("npm:body-parser");
-    const http = await import('node:http');
-    let htmlFile;
-    let cssFile;
-    fs.readFile('./index.html', (err, data) => {
-        if (err) {
-            throw err;
-        }
-        htmlFile = data;
-    });
-    fs.readFile('./styles.css', (err, data) => {
-        if (err) {
-            throw err;
-        }
-        cssFile = data;
-    });
-    http.createServer(
-        (req, res) => {
-            const url = req.url;
-            const method = req.method;
-            switch (url) {
-                case "/":
-                    res.writeHead(200, { 'Content-Type': 'text/html' });
-                    res.write(htmlFile);
-                    break;
-                case "/styles.css":
-                    res.writeHead(200, { 'Content-Type': 'text/css' });
-                    res.write(cssFile);
-                    break;
-                case "/clicked":
-                    res.writeHead(200, {'Content-Type': 'text/html'});
-                    return res.end("<h1>City</h1>")
-                default:
-                    res.writeHead(404, {'Content-Type': 'text/plain'})
-                    res.write("Error 404 Not Found")
-            }
-            res.end();
-        }
-    ).listen(port, () => {
-        console.log("Server running on port " + port);
-    })
-}
-
-// startServer(9000);
 import express from 'npm:express';
 import { fetchWeatherApi } from 'npm:openmeteo';
 import { CsvParseStream } from '@std/csv/parse-stream';
-
-using table = await Deno.open("./worldcities.csv", {read: true});
-const csvStream = table.readable.pipeThrough(new CsvParseStream({skipFirstRow: true}));
-const cities = await Array.fromAsync(csvStream);
-console.log(cities);
+import cities from './cities.json' with {type: "json"};
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-    res.render('index', { result: "" });
+    res.render('index', {cityname: "", result: "" });
 });
 app.get('/styles.css', (req, res) => {
     res.sendFile(import.meta.dirname + "/styles.css");
 });
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
+
     const city = req.body.city;
-    console.log(city);
-    const result = "you did it"
-    res.render('index', { result });
+    const coord = cities[city];
+
+    if (coord === undefined) {
+        const result = "Sorry, I don't know what that city is. Try another one?"
+        res.render('index', { cityname: "", result: result });
+        return
+    }
+
+    const params = {
+        latitude: coord[0],
+        longitude: coord[1],
+        current: "temperature_2m"
+    }
+    const url = "https://api.open-meteo.com/v1/forecast";
+    const responses = await fetchWeatherApi(url, params);
+    const response = responses[0];
+    const current = response.current();
+    const weatherData = {
+	    current: {
+		    temperature_2m: current.variables(0).value(),
+	    },
+    };
+    const result = `${Math.round(weatherData.current.temperature_2m)}°C`
+    res.render('index', { cityname: city, result: result });
 });
 
 app.listen(9000);
--- table.py
+++ table.py
@@ -1,11 +1,11 @@
 import csv
-
+import json
 filename = "worldcities.csv"
 d = {}
 with open(filename, 'r', newline='', encoding='utf-8') as f:
     reader = csv.reader(f, delimiter=',')
     next(reader) # toss headers
-    for (_, city, lat, lng, _, _, _, _, _, _, _) in reader:
+    for (city, _, lat, lng, _, _, _, _, _, _, _) in reader:
         d.setdefault(city, []).extend([lat, lng])
 
-print(d)
+print(json.dumps(d))
